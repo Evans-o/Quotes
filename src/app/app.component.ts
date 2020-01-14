18cae87ac4b138edb97e5a14e374ecd0d089c8a6
Evans-o <evansopindi@yahoo.com>
2020-01-14 17:24:05 +0300

    styled body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote []= [
-    new Quote(1, 'Denis', 'Robert frost', 'In three words i can sum up everything i have learned about life:It goes on',new Date(2020,3,14)),
-    new Quote(2,'Brian', 'Oscar wild', 'To live is the rarest thing in the world. most people exist ,that is all.',new Date(2019,1,20)),
-    new Quote(3,'ann','J.k wrolings', 'It does not do well on drams and forget to live',new Date(2020,9,23)),
-    new Quote(4,'lennox','Marktwain', 'Good friends,good books and sleepy concience is the ideal of life.',new Date(2019,2,14)),
-    new Quote(5,'mercy','Allien seunders','Life is what happens to us while we are making other plans',new Date(2020,6,12)),
-    new Quote(6,'Evans','Evans','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote(1, 'Denis', 'Robert frost', 0,0,'In three words i can sum up everything i have learned about life:It goes on',new Date(2020,3,14)),
+    new Quote(2,'Brian', 'Oscar wild', 0,0, 'To live is the rarest thing in the world. most people exist ,that is all.',new Date(2019,1,20)),
+    new Quote(3,'ann','J.k wrolings',  0,0,'It does not do well on drams and forget to live',new Date(2020,9,23)),
+    new Quote(4,'lennox','Marktwain',  0,0,'Good friends,good books and sleepy concience is the ideal of life.',new Date(2019,2,14)),
+    new Quote(5,'mercy','Allien seunders', 0,0,'Life is what happens to us while we are making other plans',new Date(2020,6,12)),
+    new Quote(6,'Evans',  'Evans', 0,0,'Cause I am an evil overlord',new Date(2020,3,14)),
   ];
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,12 @@
+
+
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number,public name: string, public author: string, public description: string,public completeDate: Date){
+
+    constructor(public id: number,public name: string, public author:string, public counter1: number, public counter2: number,public description: string, public completeDate: Date){
       this.showDescription=false;
+      this.counter1 = 0;
+      this.counter2 = 0;
       
     }
   }
